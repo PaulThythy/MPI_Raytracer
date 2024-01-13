3fe676628751b701ec4833180156e8d950cd690a
PaulThythy <paul.thi@hotmail.fr>
2024-01-13 02:24:31 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MPI_Raytracer
+
+## compile a simple MPI program :
+```sh
+mpicxx src/main.cpp -o bin/main
+```
+
+## run the program :
+```sh
+mpirun -np 4 ./main
+```
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+build_type=Debug 
+
+#Generate the Makefile using CMake
+cmake -S . -B build/$build_type -G "Ninja" -DCMAKE_BUILD_TYPE=$build_type
+cmake --build build/$build_type --target MPI_Raytracer
+
+
+#Check if the compilation was successful
+if [ $? -eq 0 ]; then
+	echo "Compilation successful. Running the program..."
+	#Execute the ParticleSystem binary
+	./build/*/MPI_Raytracer
+else
+	echo "Compilation failed. Please check the error messages."
+fi
+
+read
