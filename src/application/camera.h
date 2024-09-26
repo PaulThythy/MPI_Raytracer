b86400e143d95878606313d5599e5ef11028b54c
PaulThythy <paul.thi@hotmail.fr>
2024-09-27 01:42:45 +0200

    code cleaned

--- src/application/camera.h
+++ src/application/camera.h
@@ -10,14 +10,11 @@
 
 
 struct Camera {
-    
-    glm::vec3 m_origin;
-
     // Auxiliary vectors
     glm::vec3 m_right;
     glm::vec3 m_lowerLeftCorner;
 
-    // Informations sur le viewport
+    // viewport infos
     glm::vec3 m_viewportWidthVec;
     glm::vec3 m_viewportHeightVec;
 
@@ -52,11 +49,10 @@ struct Camera {
         m_u = glm::normalize(glm::cross(up, m_w));
         m_v = glm::cross(m_w, m_u);
 
-        m_origin = lookFrom;
         m_viewportWidthVec = m_u *  viewportWidth * focusDist;
         m_viewportHeightVec = m_v *  viewportHeight * focusDist;
 
-        m_lowerLeftCorner = m_origin - m_viewportWidthVec / 2.0f - m_viewportHeightVec / 2.0f - m_w * focusDist;
+        m_lowerLeftCorner = m_lookFrom - m_viewportWidthVec / 2.0f - m_viewportHeightVec / 2.0f - m_w * focusDist;
     }
 
     // Fonction pour obtenir un rayon en fonction des coordonnées u et v
@@ -64,9 +60,9 @@ struct Camera {
         glm::vec3 rd = Random::randomInUnitDisk() * m_lensRadius;
         glm::vec3 offset = m_u * rd.x + m_v * rd.y;
 
-        glm::vec3 direction = m_lowerLeftCorner + m_viewportWidthVec * u + m_viewportHeightVec * v - m_origin - offset;
+        glm::vec3 direction = m_lowerLeftCorner + m_viewportWidthVec * u + m_viewportHeightVec * v - m_lookFrom - offset;
 
-        return Hitable::Ray(m_origin + offset, direction);
+        return Hitable::Ray(m_lookFrom + offset, direction);
     }
 
 };
